8c5256ae604e8bc0788b418e8f7570da0dde16a9
jork41989 <jork41989@gmail.com>
2020-10-21 13:00:36 -0400

    added Rick Roll

--- features/easterEggs.js
+++ features/easterEggs.js
@@ -1,4 +1,5 @@
 module.exports = function(controller) {
+  
   if (controller.adapter.name === 'Web Adapter') {
 
     controller.hears(['nyan cat'], 'message', (bot, message) => {
@@ -10,7 +11,36 @@ module.exports = function(controller) {
               }
           ]
       })
-  })
+    })
+
+    controller.hears(['numa numa'], 'message', (bot, message) => {
+      bot.reply(message, {
+          files: [
+              {
+                  url: 'https://media.giphy.com/media/kh8ePQvhzWL5e/giphy.gif',
+                  image: true
+              }
+          ]
+      })
+    })
+
+
+    controller.hears(['Rick Roll'], 'message', (bot, message) => {
+      
+      bot.reply(message, "Never gonna give you up")
+      bot.reply(message, "Never gonna let you down")
+      bot.reply(message, {
+          files: [
+              {
+                  url: 'https://media.giphy.com/media/g7GKcSzwQfugw/giphy.gif',
+                  image: true
+              }
+          ]
+      })
+   
+    
+    })
+    
 
   }
 }
