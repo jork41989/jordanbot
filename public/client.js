50e9a823ae5c892961e18bdb0ad6d9aa17bdcc03
jork41989 <jork41989@gmail.com>
2020-10-26 19:53:52 -0400

    added npm for showdown

--- public/client.js
+++ public/client.js
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
+var showdown  = require('showdown'),
 var converter = new showdown.Converter();
 converter.setOption('openLinksInNewWindow', true);
 
